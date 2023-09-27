6d128e7a4dcf7de7c7a1c6cb2fcd9a35725dd0be
tajemouti <tajemouti@gmail.com>
2023-09-27 14:02:04 +0100

    Write sql queries

--- queries.sql
+++ queries.sql
@@ -29,3 +29,10 @@ UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO sp1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, COUNT(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg),MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
