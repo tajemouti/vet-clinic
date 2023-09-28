5a28c5f30c2bb1c90f6697d11de7e30bdb4a2957
tajemouti <tajemouti@gmail.com>
2023-09-28 15:56:04 +0100

    Create the species table

--- schema.sql
+++ schema.sql
@@ -11,3 +11,16 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(50);
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
