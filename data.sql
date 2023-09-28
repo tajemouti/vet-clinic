e6957d0108a666b70df6570492d816e9685e5a45
tajemouti <tajemouti@gmail.com>
2023-09-28 16:12:11 +0100

    Insert data into the owners table

--- data.sql
+++ data.sql
@@ -14,3 +14,11 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 ('Boarmon', '2005-06-07', 7, true, 20.40),
 ('Blossom', '1998-10-13', 3, true, 17.00),
 ('Ditto', '2022-05-14', 4, true, 22.00);
+
+INSERT INTO owners (full_name, age) VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob' , 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
