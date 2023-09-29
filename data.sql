50f667477ade6aa5c2f280a5d837a51bca4db901
tajemouti <tajemouti@gmail.com>
2023-09-29 04:34:17 +0100

    Insert data into the specializations table

--- data.sql
+++ data.sql
@@ -52,3 +52,9 @@ INSERT INTO vets (name, age, date_of_graduation) VALUES
 ('Maisy Smith', 26, '2019-01-17'),
 ('Stephanie Mendez', 64, '1981-05-04'),
 ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations VALUES
+((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM species WHERE name = 'Pokemon')),
+((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Pokemon')),
+((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Pokemon')),
+((SELECT id FROM vets WHERE name = 'Jack Harkness'), (SELECT id FROM species WHERE name = 'Digimon'));
